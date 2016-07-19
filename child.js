0a6135275b717d79d09391b3e3952df142a70cc0
Daniel Grace <negativeview@gmail.com>
2016-07-19 17:13:21 +0000

    Add in some child debugging.

--- child.js
+++ child.js
@@ -1,1 +1,7 @@
-console.log('in child');
+process.on('uncaughtException', (err) => {
+	console.log('got uncaught exception', err);
+});
+
+process.on('message', (m) => {
+	console.log('got message', m);
+});
