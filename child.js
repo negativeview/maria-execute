ad3c170882e0e4070adcceb21b650f6879092ef5
Daniel Grace <negativeview@gmail.com>
2016-08-01 15:10:30 +0000

    Log exceptions before serializing.

--- child.js
+++ child.js
@@ -3,6 +3,7 @@ const Serialize = require('maria-serialize');
 const vm = require('vm');
 
 process.on('uncaughtException', (err) => {
+	console.log('uncaught', err);
 	process.send({
 		type: 'exception',
 		exception: Serialize.serialize(err)
